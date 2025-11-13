191eab46c4161012995d81a09d4541e9a050f10c
Jonathan Aldas <jonaaldas@gmail.com>
2025-11-13 15:06:36 +0100

    init

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <UPage>
+    <UContainer> </UContainer>
+  </UPage>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
+import { env } from "./env";
+console.log(env);
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/image", "@nuxt/ui"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["./app/styles/index.css"],
+  devServer: {
+    port: 9250,
+  },
+  convex: {
+    url: env.CONVEX_URL,
+  },
+});
